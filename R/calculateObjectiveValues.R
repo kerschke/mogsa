b58968832ff3bff0ae10e68fc0d98e74a5b192b0
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2019-12-09 11:51:22 +0100

    Parallelize apply using future package

--- R/calculateObjectiveValues.R
+++ R/calculateObjectiveValues.R
@@ -1,9 +1,17 @@
 #' @export
-calculateObjectiveValues = function(points, fn) {
+calculateObjectiveValues = function(points, fn, parallelize = FALSE) {
   n = smoof::getNumberOfObjectives(fn)
-  names = sapply(1:n, function(i) paste0("y", i))
+  names = paste0("y", 1:n)
   
-  obj.space = apply(points, 1, fn)
+  if (parallelize) {
+    # setup should occur somewhere else? see documentation for future::plan
+    future::plan(future::multisession, workers = (future::availableCores() - 1))
+    obj.space = future.apply::future_apply(points, 1, fn)
+    future::plan(future::sequential)
+  } else {
+    obj.space = apply(points, 1, fn)
+  }
+  
   obj.space = as.data.frame(t(obj.space))
   names(obj.space) = names
   
