8c825397f8cac8885d7feeb5d0c5cc75c0be8ff7
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2020-01-03 18:34:10 +0100

    Document usage of future.apply

--- R/zzz.R
+++ R/zzz.R
@@ -5,6 +5,7 @@
 #' @import plotly
 #' @import kableExtra
 #' @import smoof
+#' @import future.apply
 #' @importFrom graphics abline arrows contour grid image plot points
 #' @importFrom grDevices axisTicks terrain.colors
 #' @importFrom Rcpp evalCpp
