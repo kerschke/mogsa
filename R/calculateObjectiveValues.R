5d62a45e82069a294f40900cfb7b570057e9fb70
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2020-01-08 23:42:35 +0100

    Better parallelization of objective value calculation

--- R/calculateObjectiveValues.R
+++ R/calculateObjectiveValues.R
@@ -1,18 +1,28 @@
 #' @export
-calculateObjectiveValues = function(points, fn, parallelize = FALSE) {
+calculateObjectiveValues = function(points, fn, parallelize = FALSE, parallel.cores = (parallel::detectCores() - 1)) {
   n = smoof::getNumberOfObjectives(fn)
   names = paste0("y", 1:n)
   
+  cat("Evaluating grid of objective values ...\n")
+  
   if (parallelize) {
-    # setup should occur somewhere else? see documentation for future::plan
-    future::plan(future::multisession, workers = (future::availableCores() - 1))
-    obj.space = future.apply::future_apply(points, 1, fn)
-    future::plan(future::sequential)
+    cuts = cut(1:nrow(points), parallel.cores)
+    r = parallel::mclapply(levels(cuts), function(i) {
+      t(apply(points[cuts == i,], 1, fn))
+    }, mc.cores = parallel.cores)
+    
+    # TODO: Which one is really faster?
+    # r = parallel::mclapply(seq_row(points), function(i) {
+    #   as.numeric(fn(points[i,]))
+    # }, mc.cores = parallel.cores)
+    
+    obj.space = as.matrix(do.call(rbind, r))
+
+    
   } else {
-    obj.space = apply(points, 1, fn)
+    obj.space = t(apply(points, 1, fn))
   }
   
-  obj.space = t(obj.space)
   colnames(obj.space) = names
   
   return(obj.space)
