3a9b8079016d007a4c9f5d2e6d392ed4e1c0245d
Lennart Schäpermeier <schaepermeier@uni-muenster.de>
2020-01-12 17:45:19 +0100

    Inlcude shinyjs for simple animation and further improvements:
    - Include option to upload/download grid data
    - Decouple grid calculation from plotting
    - Fix "Update Plot" not responding

--- R/dashboard/app.R
+++ R/dashboard/app.R
@@ -1,14 +1,33 @@
+library(shiny)
+library(shinyjs)
+
 source("functions.R")
 
+# Maximum Upload Size = 50MB
+# Might need to be changed for larger files
+options("shiny.maxRequestSize" = 50 * (1024**2))
+
 ui <- fluidPage(
+  useShinyjs(),
   h1("Mogsa Dashboard <Working Title>"),
   sidebarLayout(
     sidebarPanel(
       selectInput("fn.id", "Multi-objective function", c("Select a function"="",test.function.ids)),
+      tabsetPanel(
+        tabPanel("Calculate Grid",
+          numericInput("grid.size", "Grid size", 100, min=20, max=3000, step=1),
+          actionButton("update.grid", "Calculate Grid"),
+          downloadButton('download.grid', "Download Grid"),
+        ),
+        tabPanel("Upload Grid",
+          fileInput('upload.grid', "Upload Grid", accept = c(".Rds"))
+        ),
+        id = "grid.tabs",
+        type = "pills"
+      ),
       selectInput("plot.type", "Type of plot", c("Select a function first" = "")),
-      numericInput("grid.size", "Grid size", 100, min=20, max=300, step=1),
       selectInput("space", "Select space to plot", c("Decision Space"="decision.space", "Objective Space"="objective.space", "Decision + Objective Space"="both")),
-      actionButton("update.plot", "Update Plot")
+      actionButton("update.plot", "Update Plot", class="btn-primary")
     ),
     mainPanel(
       plotly::plotlyOutput(outputId = "plot", height = "100%")
@@ -17,17 +36,74 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
+  # initially, hide all these elements:
+  hide('grid.size')
+  hide('grid.tabs')
+  hide('update.grid')
+  hide('download.grid')
+  hide('upload.grid')
+  hide('plot.type')
+  hide('space')
+  hide('update.plot')
+  
+  grid = NULL
+  
   observe({
     fn = test.functions[[as.numeric(input$fn.id)]]
-    if (is.null(fn)) return()
+    
+    if (is.null(fn)) {
+      return()
+    }
+    
+    hide('plot.type')
+    hide('update.plot')
+    hide('space')
+    
+    disable('download.grid')
+    
+    grid <<- NULL
+    
+    show('grid.tabs')
+    show('grid.size')
+    show('update.grid')
+    show('download.grid')
+    show('upload.grid')
     
     if (smoof::getNumberOfParameters(fn) == 2) {
-      updateSliderInput("grid.size", session = session, value = 100, min=20, max=500, step=1)
+      updateSliderInput("grid.size", session = session, value = 100, min=20, max=3000, step=1)
       updateSelectInput(session = session, inputId = "plot.type", choices = list("Heatmap" = "heatmap"))
     } else {
-      updateSliderInput("grid.size", session = session, value = 50, min=20, max=100, step=1)
-      updateSelectInput(session = session, inputId = "plot.type", choices = list("Onion Layers" = "layers", "MRI Scan" = "scan", "Pareto Front" = "pareto"))
+      updateSliderInput("grid.size", session = session, value = 50, min=20, max=200, step=1)
+      updateSelectInput(session = session, inputId = "plot.type", choices = list("Onion Layers" = "layers", "MRI Scan" = "scan", "Nondominated" = "pareto"))
     }
+  })
+  
+  update.grid = function() {
+    fn = test.functions[[as.numeric(input$fn.id)]]
+    if (is.null(fn)) {
+      return()
+    }
+    
+    grid <<- mogsa::generateDesign(fn, input$grid.size)
+    grid$obj.space <<- mogsa::calculateObjectiveValues(grid$dec.space, fn, parallelize = T)
+    
+    gradients.fn <<- mogsa::computeGradientFieldGrid(grid, fn, prec.norm = 0)
+    grid$height <<- mogsa::computeCumulatedPathLengths(grid$dec.space, gradients.fn, fix.diagonals = T)
+  }
+  
+  observeEvent(input$update.grid, {
+    disable('update.grid')
+    disable('update.plot')
+    
+    update.grid()
+    
+    show('plot.type')
+    show('space')
+    show('update.plot')
+    
+    enable('update.grid')
+    enable('update.plot')
+    enable('download.grid')
   })
   
   get.plot = function() {
@@ -38,12 +114,6 @@ server <- function(input, output, session) {
     
     d = smoof::getNumberOfParameters(fn)
     n = smoof::getNumberOfObjectives(fn)
-    
-    grid = mogsa::generateDesign(fn, input$grid.size)
-    grid$obj.space = mogsa::calculateObjectiveValues(grid$dec.space, fn, parallelize = T)
-    
-    gradients.fn = mogsa::computeGradientFieldGrid(grid, fn)
-    grid$height = mogsa::computeCumulatedPathLengths(grid$dec.space, gradients.fn, fix.diagonals = T)
     
     if (d == 2) {
       mogsa::plotly2DHeatmap(grid, fn, mode=input$space)
@@ -59,9 +129,49 @@ server <- function(input, output, session) {
   
   output$plot = plotly::renderPlotly(
     eventReactive(input$update.plot, {
-      get.plot()
-    })()
+      disable('update.grid')
+      disable('update.plot')
+      
+      options(warn = -1)
+      p = get.plot()
+      
+      enable('update.grid')
+      enable('update.plot')
+      
+      p
+    }, event.quoted = T)()
   )
+  
+  output$download.grid = downloadHandler(
+    filename = function() {
+      fn = test.functions[[as.numeric(input$fn.id)]]
+      paste0(smoof::getName(fn), "-grid-", input$grid.size, '.Rds')
+    },
+    
+    content = function(file) {
+      saveRDS(grid, file)
+    }
+  )
+  
+  observeEvent(input$upload.grid, {
+    if (!endsWith(input$upload.grid$datapath, ".Rds")) {
+      print(input$upload.grid)
+      return()
+    }
+    
+    disable('update.grid')
+    disable('update.plot')
+    
+    grid <<- readRDS(input$upload.grid$datapath)
+    
+    show('plot.type')
+    show('space')
+    show('update.plot')
+    
+    enable('update.grid')
+    enable('update.plot')
+    enable('download.grid')
+  })
 }
 
 shinyApp(ui, server)
